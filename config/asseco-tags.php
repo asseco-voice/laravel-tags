53faa05517bc87cffe59e3cb83eb948b1929c23b
Marko Dupor <marko.dupor@gmail.com>
2021-03-09 14:33:23 +0100

    Removed unnecessary things from config

--- config/asseco-tags.php
+++ config/asseco-tags.php
@@ -2,22 +2,6 @@
 
 declare(strict_types=1);
 
-use Asseco\Tags\App\Models\Tag;
-use Asseco\Tags\App\Traits\Taggable;
-
 return [
-    /**
-     * Path to Laravel models in 'path => namespace' format.
-     *
-     * This does not recurse in folders, so you need to specify
-     * an array of paths if non-standard models are to be used
-     */
-    'models_path' => [
-        app_path('Models') => 'App\\Models\\',
-    ],
 
-    /**
-     * Namespace to Taggables trait.
-     */
-    'trait_path'  => Taggables::class,
 ];
