35ee3f967ac4474c0a484715640a62f02a8c1652
Marko Dupor <marko.dupor@gmail.com>
2021-03-31 10:14:05 +0200

    Cleanup

--- src/TagsServiceProvider.php
+++ src/TagsServiceProvider.php
@@ -13,7 +13,6 @@ class TagsServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/asseco-tags.php', 'asseco-tags');
         $this->loadMigrationsFrom(__DIR__ . '/../migrations');
         $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
     }
@@ -23,6 +22,6 @@ class TagsServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->publishes([__DIR__ . '/../config/asseco-tags.php' => config_path('asseco-tags.php')]);
+        //
     }
 }
